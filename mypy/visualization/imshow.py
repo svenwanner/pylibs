ae42c3e5633a9a573cc0ed212b15e41d76973d61
Sven Wanner <sven.wanner@iwr.uni-heidelberg.de>
2014-05-09 14:52:43 +0200

    overlay show implemented

--- mypy/visualization/imshow.py
+++ mypy/visualization/imshow.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-
+import vigra
 import numpy as np
+
 import pylab as plt
 
-def imshow(im,cmap="gray"):
+
+def imshow(im, cmap="gray"):
     if cmap == "gray": 
         cmap = plt.cm.gray
     elif cmap == "jet": 
@@ -16,7 +17,59 @@ def imshow(im,cmap="gray"):
         
     amax = np.amax(im)
     amin = np.amin(im)
-    
-    plt.imshow(im,cmap=cmap)
+
+    plt.imshow(im, cmap=cmap)
     plt.title("range: ("+str(amin)+","+str(amax)+")")
     plt.show()
+
+
+def imoverlay(im, overlay, alpha=0.5, show=True):
+    assert isinstance(im, np.ndarray)
+    assert isinstance(overlay, np.ndarray)
+    assert isinstance(alpha, float)
+    assert isinstance(show, bool)
+
+    assert 0.0 < alpha < 1.0
+    assert im.shape[0] == overlay.shape[0]
+    assert im.shape[1] == overlay.shape[1]
+    assert len(im.shape) == 2 or len(im.shape) == 3
+    assert len(overlay.shape) == 2 or len(overlay.shape) == 3
+
+    img = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.uint8)
+    if len(im.shape) == 2:
+        tmp = vigra.colors.linearRangeMapping(im, newRange=(0, 255)).astype(np.uint8)
+        for i in range(3):
+            img[:, :, i] = tmp[:]
+    else:
+        for i in range(3):
+            if im.shape[2] == 3:
+                tmp = vigra.colors.linearRangeMapping(im[:, :, i].astype(np.float32), newRange=(0, 255)).astype(
+                    np.uint8)
+            else:
+                tmp = vigra.colors.linearRangeMapping(im[:, :, 0], newRange=(0, 255)).astype(np.uint8)
+            img[:, :, i] = tmp[:]
+
+    olay = np.zeros((overlay.shape[0], overlay.shape[1], 3), dtype=np.uint8)
+    if len(overlay.shape) == 2:
+        tmp = vigra.colors.linearRangeMapping(overlay.astype(np.float32), newRange=(0, 255)).astype(np.uint8)
+        olay[:, :, 0] = tmp[:]
+    else:
+        if overlay.shape[2] == 3:
+            for i in range(3):
+                tmp = vigra.colors.linearRangeMapping(overlay[:, :, i].astype(np.float32), newRange=(0, 255)).astype(
+                    np.uint8)
+                olay[:, :, i] = tmp[:]
+        else:
+            tmp = vigra.colors.linearRangeMapping(overlay[:, :, 0], newRange=(0, 255)).astype(np.uint8)
+            olay[:, :, 0] = tmp[:]
+
+    img_fac = 1.0 - alpha
+    img = img[:].astype(np.float32) * img_fac + olay[:].astype(np.float32) * alpha
+    out = np.zeros_like(img).astype(np.uint8)
+
+    for i in range(3):
+        out[:, :, i] = vigra.colors.linearRangeMapping(img[:, :, i], newRange=(0, 255)).astype(np.uint8)
+
+    plt.imshow(out)
+    if show:
+        plt.show()
--- mypy/workflows/StructureTensor3D.py
+++ mypy/workflows/StructureTensor3D.py
@@ -1,5 +1,5 @@
 from mypy.lightfield import io as lfio
-from mypy.visualization.imshow import imshow
+from mypy.visualization.imshow import imoverlay
 from mypy.lightfield import helpers as lfhelpers
 from mypy.lightfield.depth import structureTensor3D as st3d
 
@@ -31,11 +31,12 @@ def compute(path, results, name, roi, inner_scale=0.6, outer_scale=1.0, focus=0)
 
     evals, evecs = st3d.structure_tensor3d(lf[:, :, :, 0], inner_scale, outer_scale)
     disparity = st3d.structure_tensor3d_conditioner(evals, evecs)
-    imshow(disparity, cmap="jet")
+    #imshow(disparity, cmap="jet")
+    imoverlay(lf[4, :, :, :], disparity, alpha=0.8)
 
 
 compute("/home/swanner/Desktop/TestLF/render/9x1/imgs_bw/",
         "/home/swanner/Desktop/TestLF/3DTensor/results",
         "pearplanepyr",
         {"size": [220, 320], "pos": [250, 320]},
-        0.6, 1.3, 5)
+        0.6, 1.3, 4)
