5f009d128260b11cce5d5b446b2f41ba17af695c
Sven Wanner <sven.wanner@iwr.uni-heidelberg.de>
2014-06-02 11:18:09 +0200

    pcl based pointcloud saving and loading works, transformation still buggy

--- mypy/pointclouds/PointCloud.py
+++ mypy/pointclouds/PointCloud.py
@@ -5,6 +5,71 @@ except Exception as e:
 
 import numpy as np
 
+
+
+class PlyWriter(object):
+    def __init__(self, name=None, cloud=None):
+
+        if name is not None:
+            assert isinstance(name, str)
+        if cloud is not None:
+            assert isinstance(cloud, Cloud)
+
+        self.name = name
+        self.cloud = cloud
+
+        if self.name is not None and self.cloud is not None:
+            self.save()
+
+
+    def save(self):
+        assert self.name is not None, "no filename set, cannot save pointcloud!"
+        assert self.cloud is not None, "no cloud set, cannot save pointcloud!"
+
+        f = open(self.name, 'w')
+        self.write_header(f)
+        self.write_points(f)
+        f.close()
+
+
+    def write_header(self, f):
+        f.write('ply\n')
+        f.write('format ascii 1.0\n')
+        f.write('element vertex %d\n' % self.cloud.points.width)
+        f.write('property float x\n')
+        f.write('property float y\n')
+        f.write('property float z\n')
+        if self.cloud.has_color:
+            self.add_color_header(f)
+        if self.cloud.has_intensities:
+            self.add_intensity_header(f)
+        if self.cloud.has_confidence:
+            self.add_confidence_header(f)
+        f.write('end_header\n')
+
+    def add_color_header(self, f):
+        f.write('property uchar red\n')
+        f.write('property uchar green\n')
+        f.write('property uchar blue\n')
+
+    def add_confidence_header(self, f):
+        f.write('property float confidence\n')
+
+    def add_intensity_header(self, f):
+        f.write('property float intensity\n')
+
+    def write_points(self, f):
+        for n in xrange(self.cloud.points.width):
+            f.write("{0} {1} {2}".format(float(self.cloud.points[n][0]), float(self.cloud.points[n][1]), float(self.cloud.points[n][2])))
+            if self.cloud.has_color:
+                f.write(" {0} {1} {2}".format(int(self.cloud.colors[n][0]), int(self.cloud.colors[n][1]), int(self.cloud.colors[n][2])))
+            if self.cloud.has_intensities:
+                f.write(" {0}".format(float(self.cloud.intensity[n])))
+            if self.cloud.has_confidence:
+                f.write(" {0}".format(float(self.cloud.confidence[n])))
+            f.write('\n')
+
+
 class Cloud(object):
 
     def __init__(self):
@@ -12,6 +77,10 @@ class Cloud(object):
         self.has_points = False
         self.colors = None
         self.has_color = False
+        self.intensities = None
+        self.has_intensities = False
+        self.confidence = None
+        self.has_confidence = False
         self.vertices = 0
 
 
@@ -20,6 +89,61 @@ class Cloud(object):
 
         if filename.endswith(".pcd"):
             self.points.to_file(filename)
+        if filename.endswith(".ply"):
+            writer = PlyWriter(filename, self)
+
+
+    def transform(self, translation=np.array([0, 0, 0]), rotation_x=0.0, rotation_y=0.0, rotation_z=0.0, atype="rad"):
+
+        assert isinstance(translation, np.ndarray), "need translation as parameter!"
+        assert isinstance(rotation_x, float), "need rotation_x as parameter!"
+        assert isinstance(rotation_y, float), "need rotation_y as parameter!"
+        assert isinstance(rotation_z, float), "need rotation_z as parameter!"
+        assert self.has_points is True, "no points available, transform failed!"
+
+        if atype != "rad":
+            rotation_x = rotation_x/180.0*np.pi
+            rotation_y = rotation_y/180.0*np.pi
+            rotation_z = rotation_z/180.0*np.pi
+
+        trans_mat = np.mat([[1, 0, 0, translation[0]], [0, 1, 0, translation[1]], [0 ,0, 1, translation[2]], [0, 0, 0, 1]], dtype=np.float32)
+        rot_mat_x = np.mat([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
+        rot_mat_y = np.mat([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
+        rot_mat_z = np.mat([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
+
+        rot_mat_x[1, 1] = np.cos(rotation_x)
+        rot_mat_x[1, 2] = -np.sin(rotation_x)
+        rot_mat_x[2, 1] = np.sin(rotation_x)
+        rot_mat_x[2, 2] = np.cos(rotation_x)
+
+        rot_mat_y[0, 0] = np.cos(rotation_y)
+        rot_mat_y[0, 2] = np.sin(rotation_y)
+        rot_mat_y[2, 0] = -np.sin(rotation_y)
+        rot_mat_y[2, 2] = np.cos(rotation_y)
+
+        rot_mat_z[0, 0] = np.cos(rotation_z)
+        rot_mat_z[0, 1] = -np.sin(rotation_z)
+        rot_mat_z[1, 0] = np.sin(rotation_z)
+        rot_mat_z[1, 1] = np.cos(rotation_z)
+
+        tmp_points = np.zeros((self.vertices, 3), dtype=np.float32)
+
+        for n in xrange(self.points.width):
+            p = np.mat([0, 0, 0, 1],dtype=np.float32)
+            p[0, 0:3] = self.points[n][:]
+
+            p = rot_mat_x * p.T
+            p = rot_mat_y * p
+            p = rot_mat_z * p
+            p = trans_mat * p
+            p = p.T
+
+            tmp_points[n, 0] = p[0, 0]
+            tmp_points[n, 1] = p[0, 1]
+            tmp_points[n, 2] = p[0, 2]
+
+        self.points.from_array(tmp_points)
+
 
     def from_file(self, filename):
         assert isinstance(filename, str)
@@ -67,8 +191,6 @@ class Cloud(object):
                 numbers = []
                 for i in line.split(" "):
                     numbers.append(float(i))
-                    if point_index < 20:
-                        print "read ", float(i)
 
                 if self.has_points:
                     for k in range(3):
@@ -81,3 +203,6 @@ class Cloud(object):
 
             self.points = pcl.PointCloud()
             self.points.from_array(tmp_points)
+
+
+
